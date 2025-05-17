cd910bec25deb907431dfa55e2368caf8175fbef
Himanshu Yadav <himanshu.yadav@giva.co>
2025-05-18 02:46:34 +0530

    firebase setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore,provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ]
 };
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: { 
+        apiKey: "AIzaSyAKr_wkqo-88V_y-AXg0Lm6IOM5ucp1uRQ",
+        authDomain: "angular-firebase-project-66e6d.firebaseapp.com",
+        projectId: "angular-firebase-project-66e6d",
+        storageBucket: "angular-firebase-project-66e6d.firebasestorage.app",
+        messagingSenderId: "70634086492",
+        appId: "1:70634086492:web:c206a39efa2e88778a4699"
+    }
+};
